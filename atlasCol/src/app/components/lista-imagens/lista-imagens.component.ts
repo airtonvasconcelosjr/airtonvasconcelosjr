1c8499145542db2ba2e67a284e4f2d4bb3dc777d
airtonvasconcelosjr <airton.vasconcelosjr@gmail.com>
2023-05-03 21:20:10 -0300

    new angular project

---
+++ atlasCol/src/app/components/lista-imagens/lista-imagens.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-imagens',
+  templateUrl: './lista-imagens.component.html',
+  styleUrls: ['./lista-imagens.component.css']
+})
+export class ListaImagensComponent {
+
+}
