fe19e3ddc54406159671634b348bc13c6bfd8e18
airtonvasconcelosjr <airton.vasconcelosjr@gmail.com>
2023-05-10 10:59:17 -0300

    Change array of colchoes

--- atlasCol/src/app/components/lista-imagens/lista-imagens.component.ts
+++ atlasCol/src/app/components/lista-imagens/lista-imagens.component.ts
@@ -12,7 +12,7 @@ export class ListaImagensComponent {
     colchao.abrirDescricao = !colchao.abrirDescricao;
   }
   
-  colchoes = [
+  king = [
     { id: 1, nome: '223', fabril:'King Koil', imagem: 'assets/imagens/223.png', descricao: 'assets/imagens/223_desc.png', abrirDescricao: false },
     { id: 2, nome: '243', fabril:'King Koil', imagem: 'assets/imagens/243.png', descricao: 'assets/imagens/243_desc.png', abrirDescricao: false },
     { id: 3, nome: '433', fabril:'King Koil', imagem: 'assets/imagens/433.png', descricao: 'assets/imagens/433_desc.png', abrirDescricao: false },
@@ -23,6 +23,9 @@ export class ListaImagensComponent {
     { id: 8, nome: '633', fabril:'King Koil', imagem: 'assets/imagens/633.png', descricao: 'assets/imagens/633_desc.png', abrirDescricao: false },
     { id: 9, nome: '763', fabril:'King Koil', imagem: 'assets/imagens/763.png', descricao: 'assets/imagens/763_desc.png', abrirDescricao: false },
     { id: 10, nome: '853', fabril:'King Koil', imagem: 'assets/imagens/853.png', descricao: 'assets/imagens/853_desc.png', abrirDescricao: false },
+    
+  ];
+  mercosul = [
     { id: 11, nome: '232', fabril:'Mercosul', imagem: 'assets/imagens/232.png', descricao: 'assets/imagens/232_desc.png', abrirDescricao: false },
     { id: 12, nome: '452', fabril:'Mercosul', imagem: 'assets/imagens/452.png', descricao: 'assets/imagens/452_desc.png', abrirDescricao: false },
     { id: 13, nome: '562', fabril:'Mercosul', imagem: 'assets/imagens/562.png', descricao: 'assets/imagens/562_desc.png', abrirDescricao: false },
@@ -33,8 +36,35 @@ export class ListaImagensComponent {
   mostrarSetaParaBaixo = true;
   mostrarMercosul = false;
   mostrarSetaParaBaixoMercosul = true;
+  mostrarRestonic = false;
+  mostrarSetaParaBaixoRestonic = true;
+  mostrarEcoflex = false;
+  mostrarSetaParaBaixoEcoflex = true
+  
 
   colchaoAberto: number | null = null;
+
+toggleMostrar(opcao: string) {
+  if (opcao === 'king') {
+    this.mostrarKing = !this.mostrarKing;
+    this.mostrarSetaParaBaixo = !this.mostrarSetaParaBaixo;
+
+    // Fechar a opção Mercosul, se estiver aberta
+    if (this.mostrarMercosul) {
+      this.mostrarMercosul = false;
+      this.mostrarSetaParaBaixoMercosul = true;
+    }
+  } else if (opcao === 'mercosul') {
+    this.mostrarMercosul = !this.mostrarMercosul;
+    this.mostrarSetaParaBaixoMercosul = !this.mostrarSetaParaBaixoMercosul;
+
+    // Fechar a opção King Koil, se estiver aberta
+    if (this.mostrarKing) {
+      this.mostrarKing = false;
+      this.mostrarSetaParaBaixo = true;
+    }
+  }
+}
 
   
   toggleDescricao(colchao: any) {
@@ -43,7 +73,8 @@ export class ListaImagensComponent {
       this.colchaoAberto = null;
     } else {
       if (this.colchaoAberto !== null) {
-        this.colchoes[this.colchaoAberto].abrirDescricao = false;
+        this.king[this.colchaoAberto].abrirDescricao = false;
+        this.mercosul[this.colchaoAberto].abrirDescricao = false;
       }
       colchao.abrirDescricao = true;
       this.colchaoAberto = colchao.id - 1;
